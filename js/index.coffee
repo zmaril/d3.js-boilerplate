1e5b4c32fae003195264f872a80e243ee0d2a5ea
Zack Maril <zack@zacharymaril.com>
2012-06-06 11:51:00 -0200

    Added in bootstrap and changed examples

--- js/index.coffee
+++ js/index.coffee
@@ -1,9 +1,8 @@
 graphic = new Object
 
 graphic.create = ()->
-  width = $(document).width()/2
-  height = $(document).height()*.85
-  size = d3.min([width,height])
+  width = $("#graphic").width()
+  size = width
   graphic.svg = d3.select("#graphic")
     .append("svg")
       .attr("width",size)
@@ -11,11 +10,12 @@ graphic.create = ()->
 
   g = graphic.svg.append("g")
 
-  for i in _.range(size/10)
-    graphic.svg.append("rect")
-      .attr("width",size-i*10)
-      .attr("height",size-i*10)
+  circles = 40
+  for i in _.range(size/(2*circles)).reverse()
+    graphic.svg.append("circle")
       .attr("fill",if i%2 is 0 then "black" else "white")
+      .attr("r",i*circles)
+      .attr("transform","translate(#{size/2},#{size/2})")
 
 graphic.update = ()->
 
--- js/index.js
+++ js/index.js
@@ -3,17 +3,17 @@ var graphic;
 graphic = new Object;
 
 graphic.create = function() {
-  var g, height, i, size, width, _i, _len, _ref, _results;
-  width = $(document).width() / 2;
-  height = $(document).height() * .85;
-  size = d3.min([width, height]);
+  var circles, g, i, size, width, _i, _len, _ref, _results;
+  width = $("#graphic").width();
+  size = width;
   graphic.svg = d3.select("#graphic").append("svg").attr("width", size).attr("height", size);
   g = graphic.svg.append("g");
-  _ref = _.range(size / 10);
+  circles = 40;
+  _ref = _.range(size / (2 * circles)).reverse();
   _results = [];
   for (_i = 0, _len = _ref.length; _i < _len; _i++) {
     i = _ref[_i];
-    _results.push(graphic.svg.append("rect").attr("width", size - i * 10).attr("height", size - i * 10).attr("fill", i % 2 === 0 ? "black" : "white"));
+    _results.push(graphic.svg.append("circle").attr("fill", i % 2 === 0 ? "black" : "white").attr("r", i * circles).attr("transform", "translate(" + (size / 2) + "," + (size / 2) + ")"));
   }
   return _results;
 };
